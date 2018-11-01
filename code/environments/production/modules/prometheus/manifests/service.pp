f20f36acafcf687ad9685b00fbf76df456e932a6
xyr115 <42252570+xyr115@users.noreply.github.com>
2018-11-01 15:38:41 -0400

    Add a notify to the prometheus service manifest

--- code/environments/production/modules/prometheus/manifests/service.pp
+++ code/environments/production/modules/prometheus/manifests/service.pp
@@ -1,11 +1,12 @@
 class prometheus::service {
     file { '/etc/systemd/system/prometheus.service':
-        source => 'puppet:///modules/prometheus/prometheus.service',
+      notify => Service['prometheus'],
+      source => 'puppet:///modules/prometheus/prometheus.service',
     }
 
     service { 'prometheus':
-        ensure  => running,
-        enable  => true,
-        require => Package['prometheus'],
+      ensure  => running,
+      enable  => true,
+      require => Package['prometheus'],
     }
 }
